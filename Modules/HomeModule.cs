25e5523b7ed78e929132720bde28ef38645c3493
Minh Phuong and Renee Mei <minh+renee@>
2017-02-08 10:46:57 -0800

    add on places.cshtml, call GetNumber and GetName on instance visited, create new Get route for places/{number}, create place.cshtml for individual place

--- Modules/HomeModule.cs
+++ Modules/HomeModule.cs
@@ -1,6 +1,6 @@
 using Nancy;
-using VisitedPlaces.Objects
-using System.Collections.Generic
+using VisitedPlaces.Objects;
+using System.Collections.Generic;
 
 namespace VisitedPlaces
 {
@@ -9,7 +9,20 @@ namespace VisitedPlaces
     public HomeModule()
     {
       Get["/"] = _ => View["index.cshtml"];
+      Get["/places"] = _ => {
+        List<Place> allPlace = Place.GetList();
+        return View["places.cshtml", allPlace];
+      };
       Get["/place_form"] = _ => View["place_form.cshtml"];
+      Post["/places"] = _ => {
+        Place newPlace = new Place(Request.Form["new-place"]);
+        List<Place> allPlace = Place.GetList();
+        return View["places.cshtml", allPlace];
+      };
+      Get["/places/{number}"] = parameters => {
+        Place numberedPlace = Place.Find(parameters.number);
+        return View["/place.cshtml", numberedPlace];
+      };
     }
   }
 }
